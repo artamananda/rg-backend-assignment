ddba16d61139fd6ac1dd16d60cd8e6f04705c8ea
Artamananda <artamananda@gmail.com>
2022-10-14 19:40:58 +0700

    feat: add http-server-chaining-middleware

---
+++ golang/http-server-chaining-middleware-cp-v2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+)
+
+func AdminHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to Admin page"))
+	}
+}
+
+func RequestMethodGetMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(405)
+			w.Write([]byte("Method is not allowed"))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func AdminMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		role := r.Header.Get("role")
+		if role != "ADMIN" {
+			w.WriteHeader(401)
+			w.Write([]byte("Role not authorized"))
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	mux := http.DefaultServeMux
+
+	mux.HandleFunc("/admin", AdminHandler())
+
+	var handler http.Handler = mux
+	handler = AdminMiddleware(handler)
+	handler = RequestMethodGetMiddleware(handler)
+
+	http.ListenAndServe("localhost:8080", handler)
+}
