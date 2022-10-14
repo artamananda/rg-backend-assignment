08bbc16bfd01f28f61e0cfcafec7434c11ec21de
Artamananda <artamananda@gmail.com>
2022-10-14 16:54:20 +0700

    feat: add http-server-handler-middleware

--- golang/http-server-handler-middleware-cp-v1/main.go
+++ golang/http-server-handler-middleware-cp-v1/main.go
@@ -12,11 +12,24 @@ func StudentHandler() http.HandlerFunc {
 }
 
 func RequestMethodGet(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}) // TODO: replace this
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(405)
+			w.Write([]byte("Method is not allowed"))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-	// TODO: answer here
+	mux := http.DefaultServeMux
 
-	http.ListenAndServe("localhost:8080", nil)
+	mux.HandleFunc("/student", StudentHandler())
+
+	var handler http.Handler = mux
+	handler = RequestMethodGet(handler)
+
+	http.ListenAndServe("localhost:8080", handler)
 }
