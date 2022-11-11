76e2abf37becb01098c1fa69c9354ab4776f04f2
Artamananda <artamananda@gmail.com>
2022-11-11 22:40:48 +0700

    add exercise-db-3

---
+++ database/exercise-db-3-v1/add_column_table_students.sql
@@ -0,0 +1,6 @@
+ALTER TABLE students ADD date_of_birth DATE NOT NULL;
+ALTER TABLE students ADD street VARCHAR(255);
+ALTER TABLE students ADD city VARCHAR(100);
+ALTER TABLE students ADD province VARCHAR(100);
+ALTER TABLE students ADD country VARCHAR(100);
+ALTER TABLE students ADD postal_code VARCHAR(50);
---
+++ database/exercise-db-3-v1/drop_column_table_students.sql
@@ -0,0 +1,4 @@
+ALTER TABLE students DROP COLUMN day_of_birth;
+ALTER TABLE students DROP COLUMN month_of_birth;
+ALTER TABLE students DROP COLUMN year_of_birth;
+ALTER TABLE students DROP COLUMN address;
